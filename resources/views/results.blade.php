86296004b5aa54687d92b17792f8a19275b31595
Tunde James <tundejames04@gmail.com>
2024-06-14 16:20:12 +0100

    Implement the search functionality

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
   <x-page-heading>Results</x-page-heading>
 
-  <div class="mt-6 space-y-6">
+  <div class="space-y-6">
     @foreach ($jobs as $job)
     <x-job-card-wide :$job />
     @endforeach
