b10dd2466c7bca28a15878b61a5d1cc47297134f
Tunde James <tundejames04@gmail.com>
2024-06-14 16:18:52 +0100

    Craete the register and login form
    Implement the register and login functionality.

--- resources/views/components/forms/button.blade.php
+++ resources/views/components/forms/button.blade.php
@@ -1,3 +1,3 @@
 <button {{ $attributes(['class' => 'bg-blue-800 rounded py-2 px-6 font-bold']) }}>
-  {{ $slot }
-}</button>
+  {{ $slot }}
+</button>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,7 +10,7 @@
   @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="bg-black text-white font-hanken-grotesk">
+<body class="bg-black text-white font-hanken-grotesk pb-20">
   <div class="px-10">
     <nav class="flex justify-between items-center py-4 border-b border-white/10">
       <div>
@@ -26,9 +26,18 @@
         <a href="#">Companies</a>
       </div>
 
+      @auth
       <div>
-        <a href="#">Post a Job</a>
+        <a href="/jobs/create">Post a Job</a>
       </div>
+      @endauth
+
+      @guest
+      <div class="space-x-6 font-bold">
+        <a href="/register">Sign Up</a>
+        <a href="/login">Login</a>
+      </div>
+      @endguest
     </nav>
 
     <main class="mt-10 max-w-[986px] mx-auto">
