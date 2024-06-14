f9302452fd207a424ad67b864511f2dcf8cbf17b
Tunde James <tundejames04@gmail.com>
2024-06-14 17:37:40 +0100

    Cleanup the files
    Updated the query to avoid N + 1 problem

--- resources/views/components/employer-logo.blade.php
+++ resources/views/components/employer-logo.blade.php
@@ -1,4 +1,4 @@
-@props(['width'=> 90])
+@props(['employer', 'width'=> 90])
 
 
-<img src="http://picsum.photos/seed/{{ rand(0, 100000) }}/{{ $width }}" alt="" class="rounded-xl">
+<img src="{{ asset($employer->logo) }}" alt="" width="{{ $width }}" class="rounded-xl">
